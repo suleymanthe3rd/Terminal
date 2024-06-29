d2612800aa182a13fb1f14b1fd6b8bd50a9d1498
Sasidu Sandakalum <84926805+emusksasindu@users.noreply.github.com>
2024-06-29 16:09:25 +0530

    Update MatrixRain.jsx

--- src/components/MatrixRain.jsx
+++ src/components/MatrixRain.jsx
@@ -2,48 +2,47 @@ import { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { primary } from '../utils/colors';
 
-
 const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-
   background-color: transparent;
   overflow: hidden;
 `;
 
 const MatrixText = styled.div`
-  font-size: 8px;
+  font-size: 10px;
   font-weight: bold;
   font-family: 'monospace';
   color: ${primary};
-  display: flex; /* Make MatrixText a flex container */
+  display: flex;
 `;
 
 const Column = styled.div`
   display: flex;
-  flex-direction: column; 
-  gap: 1px; 
+  flex-direction: column;
+  gap: 1px;
   margin-right: 10px;
 `;
 
 const Char = styled.span`
   ${({ opacity }) => `
     opacity: ${opacity};
+    transition: opacity 0.2s ease-in-out;
   `}
 `;
 
 const MatrixRain = () => {
   const [matrix, setMatrix] = useState([]);
-  const rows = 80;
-  const cols = 80;
+  const rows = 40;
+  const cols = 20;
   const animationRef = useRef(null);
-  const delayRef = useRef(50); // initial delay
+  const delayRef = useRef(50);
 
   useEffect(() => {
     setMatrix(
-      Array.from({ length: cols }, () => 
-        Array.from({ length: rows }, () => ({ char: ' ', opacity: 0 })) // Initialize with space characters and 0 opacity
+      Array.from({ length: cols }, () =>
+        Array.from({ length: rows }, () => ({ char: ' ', opacity: 0 }))
       )
     );
   }, []);
@@ -54,16 +53,33 @@ const MatrixRain = () => {
   }, []);
 
   const updateMatrix = () => {
-    setMatrix((prevMatrix) =>
-      prevMatrix.map((column) => {
-        const newColumn = [...column];
-        newColumn.pop(); 
-        newColumn.unshift({ char: randomChar(), opacity: randomOpacity() }); 
-        return newColumn;
-      })
+    const updatedColumns = Math.floor(cols / 6); // Number of columns to update
+
+    setMatrix((prevMatrix) => {
+      const newMatrix = [...prevMatrix];
+
+      // Update the specified number of columns randomly
+      for (let i = 0; i < updatedColumns; i++) {
+        const randomColumnIndex = Math.floor(Math.random() * cols);
+        newMatrix[randomColumnIndex] = updateColumn(
+          newMatrix[randomColumnIndex]
+        );
+      }
+      return newMatrix;
+    });
+
+    delayRef.current = 10;
+    animationRef.current = setTimeout(
+      () => requestAnimationFrame(updateMatrix),
+      delayRef.current
     );
-    delayRef.current = Math.random() * 200 + 20; 
-    animationRef.current = setTimeout(() => requestAnimationFrame(updateMatrix), delayRef.current);
+  };
+
+  const updateColumn = (column) => {
+    const newColumn = [...column];
+    newColumn.pop();
+    newColumn.unshift({ char: randomChar(), opacity: randomOpacity() });
+    return newColumn;
   };
 
   const randomChar = () =>
@@ -71,16 +87,18 @@ const MatrixRain = () => {
 
   const randomOpacity = () => {
     const opacity = Math.random();
-    return opacity < 0.2 ? 0.2 : 0.6; 
+    return opacity < 0.2 ? 0.2 : 0.8;
   };
 
   return (
     <Container>
       <MatrixText>
-        {matrix.map((column, colIndex) => ( // Render each column
+        {matrix.map((column, colIndex) => (
           <Column key={colIndex}>
             {column.map((char, rowIndex) => (
-              <Char key={rowIndex} opacity={char.opacity}>{char.char}</Char>
+              <Char key={rowIndex} opacity={char.opacity}>
+                {char.char}
+              </Char>
             ))}
           </Column>
         ))}
